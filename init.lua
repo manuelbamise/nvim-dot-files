70e07ad627f71ae409f7d5d23399b135dd30f4f7
manuelbamise <etobi1930@gmail.com>
2025-08-21 14:39:19 +0100

    process: successfully configured the auto pairs plugin

--- init.lua
+++ init.lua
@@ -1,1 +1,4 @@
 require("config.lazy")
+
+require("config.options")
+-- require("plugins.echaMiniPairs").setup()
---
+++ lua/config/options.lua
@@ -0,0 +1,16 @@
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- tabs & spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- appearance
+vim.opt.termguicolors = true
+
