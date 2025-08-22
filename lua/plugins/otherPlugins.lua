e3c01a1c2a60cbbae612bc83924dae86676c07bb
manuelbamise <etobi1930@gmail.com>
2025-08-22 09:13:01 +0100

    process: routine save

--- lua/plugins/otherPlugins.lua
+++ lua/plugins/otherPlugins.lua
@@ -29,9 +29,7 @@ return {
 
   -- LSP + Autocomplete
   { "neovim/nvim-lspconfig" },
-  { "williamboman/mason.nvim", config = true },
-  { "williamboman/mason-lspconfig.nvim" },
-  { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/nvim-cmp" },
   { "hrsh7th/cmp-nvim-lsp" },
   { "L3MON4D3/LuaSnip" },
 
