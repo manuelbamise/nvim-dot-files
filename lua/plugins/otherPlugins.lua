1d7d2073451ad69db3a5377ea8c9579eb0c5896c
manuelbamise <etobi1930@gmail.com>
2025-08-22 10:05:23 +0100

    process: configured the opening dashboard for neovim

--- lua/plugins/otherPlugins.lua
+++ lua/plugins/otherPlugins.lua
@@ -1,40 +1,49 @@
 return {
-  -- Dashboard (custom opening page)
-  { "glepnir/dashboard-nvim", event = "VimEnter",
-    config = function()
-      require('dashboard').setup {
-        theme = 'doom',
-        config = {
-          header = { "\nThis is my dojo.." },
-          center = {
-            { desc = "Find File", action = "Telescope find_files" },
-            { desc = "Recent Files", action = "Telescope oldfiles" },
-            { desc = "Open Config", action = "edit ~/.config/nvim/init.lua" },
-          },
-        }
-      }
-    end
-  },
+	-- -- Dashboard (custom opening page)
+	-- {
+	-- 	"glepnir/dashboard-nvim",
+	-- 	event = "VimEnter",
+	-- 	config = function()
+	-- 		require("dashboard").setup({
+	-- 			theme = "doom",
+	-- 			config = {
+	-- 				header = { "This is my dojo.." },
+	-- 				center = {
+	-- 					{ desc = "Find File", action = "Telescope find_files" },
+	-- 					{ desc = "Recent Files", action = "Telescope oldfiles" },
+	-- 					{ desc = "Open Config", action = "edit ~/.config/nvim/init.lua" },
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
+	{
 
-  -- Telescope (fuzzy finder)
-  { "nvim-telescope/telescope.nvim", keys={
-      {'tf',':Telescope find_files<CR>', desc='telescope find files', silent= true},
-{'tg',':Telescope live_grep<CR>', desc='telescope live live_grep', silent= true},
+		"goolord/alpha-nvim",
+		config = function()
+			require("alpha").setup(require("alpha.themes.dashboard").config)
+		end,
+	},
+	--
+	-- Telescope (fuzzy finder)
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
+			{ "tf", ":Telescope find_files<CR>", desc = "telescope find files", silent = true },
+			{ "tg", ":Telescope live_grep<CR>", desc = "telescope live live_grep", silent = true },
+		},
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-      
-  }, dependencies = { "nvim-lua/plenary.nvim" } },
+	-- Treesitter (better syntax highlighting)
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
-  -- Treesitter (better syntax highlighting)
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	-- LSP + Autocomplete
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "L3MON4D3/LuaSnip" },
 
-  -- LSP + Autocomplete
-  { "neovim/nvim-lspconfig" },
-    { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "L3MON4D3/LuaSnip" },
-
-  -- Git signs
-  { "lewis6991/gitsigns.nvim", config = true },
-
+	-- Git signs
+	{ "lewis6991/gitsigns.nvim", config = true },
 }
-
