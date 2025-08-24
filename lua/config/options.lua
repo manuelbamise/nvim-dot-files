a81cacab1c832ff12f895447e775240a592f30b5
manuelbamise <etobi1930@gmail.com>
2025-08-24 07:34:35 +0100

    process: redefined the tab keymaps for the config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,7 +4,7 @@ vim.opt.relativenumber = true
 
 -- tabs & spaces
 vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 -- search
@@ -13,4 +13,3 @@ vim.opt.smartcase = true
 
 -- appearance
 vim.opt.termguicolors = true
-
